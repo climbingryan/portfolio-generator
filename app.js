898e9942a9082ea254c86ccc4496ba01442e6868
shaggy <simmons5family@gmail.com>
2020-11-19 23:10:17 -0600

    finished project. made write&copy file into promises

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const generatePage = require("./src/page-template.js")
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -138,17 +138,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./dist/index.html', pageHTML, err => {
-            if (err) throw new Error (err);
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log('Style sheet copied successfully!');
-            })
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
